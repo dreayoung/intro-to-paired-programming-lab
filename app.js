01d506b68c8a733fad16b0e9d3e236d8051d8476
dreayoung <youngmandrea@gmail.com>
2020-09-28 19:23:22 +0000

    completed all problems

--- app.js
+++ app.js
@@ -1,25 +1,51 @@
 console.log("Write your code below!")
 
 function sayHello(name){
+  console.log(`Hello, ${name}`);
 }
 
 function letterCount(name){
-
+  if(name.length > 4) {
+    console.log(`${name}, your name has ${name.length} letters. That's a pretty long name!`);
+  } else {
+    console.log(`${name}, your name has ${name.length} letters. That name's not that long!`);
+  }
 }
 
 function greetPeople(names){
-
+  names.forEach(name => {
+    sayHello(name)
+    letterCount(name)
+  })
 }
 
 function speakingGrandma(phrase){
-
+  if(phrase === phrase.toUpperCase()){
+    console.log(`NO, NOT SINCE 1938!`);
+  } else {
+    console.log(`HUH? SPEAK UP, DEAR!`);
+  }
 }
 
 
 function kebabToTitleCase(filenames){
-
+  let finalArray = [];
+  let toTitleCase = (word) => {
+    let parts = word.split('-')
+    let output = []
+  
+    parts.forEach(part => {
+      let capFirstLetter = part[0].toUpperCase()
+      let restOfWord = part.slice(1)
+      output.push(`${capFirstLetter}${restOfWord}`);
+    })
+    return output.join('');
+  }
+ for (let i = 0; i < filenames.length; i++){
+   finalArray.push(toTitleCase(filenames[i]));
+ }
+ return finalArray;
 }
-
 //Write your test cases below this comment
 
 
